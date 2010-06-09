a80cb4739455c884b779638d3afae6c67525ba76
Jeremy Bush <contractfrombelow@gmail.com>
2010-06-09 15:09:14 -0500

    Adding more security tests.

--- tests/kohana/SecurityTest.php
+++ tests/kohana/SecurityTest.php
@@ -9,16 +9,15 @@
 Class Kohana_SecurityTest extends Kohana_Unittest_TestCase
 {
 	/**
-	 * Provides test data for testEnvodePHPTags()
+	 * Provides test data for testEncodePHPTags()
 	 *
 	 * @return array Test data sets
 	 */
 	function providerEncodePHPTags()
 	{
-		return array
-			(	
-				array("&lt;?php echo 'helloo'; ?&gt;", "<?php echo 'helloo'; ?>"),
-			);
+		return array(
+			array("&lt;?php echo 'helloo'; ?&gt;", "<?php echo 'helloo'; ?>"),
+		);
 	}
 
 	/**
@@ -31,6 +30,60 @@ Class Kohana_SecurityTest extends Kohana_Unittest_TestCase
 	function testEncodePHPTags($expected, $input)
 	{
 		$this->assertSame($expected, Security::encode_php_tags($input));
+	}
+
+	/**
+	 * Provides test data for testStripImageTags()
+	 *
+	 * @return array Test data sets
+	 */
+	function providerStripImageTags()
+	{
+		return array(
+			array('foo', '<img src="foo" />'),
+		);
+	}
+
+	/**
+	 * Tests Security::strip_image_tags()
+	 *
+	 * @test
+	 * @dataProvider providerStripImageTags
+	 * @covers Security::strip_image_tags
+	 */
+	function testStripImageTags($expected, $input)
+	{
+		$this->assertSame($expected, Security::strip_image_tags($input));
+	}
+
+	/**
+	 * Provides test data for Security::token()
+	 *
+	 * @return array Test data sets
+	 */
+	function providerCSRFToken()
+	{
+		$array = array();
+		for ($i = 0; $i <= 4; $i++)
+		{
+			Security::$token_name = 'token_'.$i;
+			$array[] = array(Security::token(TRUE), Security::check(Security::token(FALSE)), $i);
+		}
+		return $array;
+	}
+
+	/**
+	 * Tests Security::token()
+	 *
+	 * @test
+	 * @dataProvider providerCSRFToken
+	 * @covers Security::token
+	 */
+	function testCSRFToken($expected, $input, $iteration)
+	{
+		Security::$token_name = 'token_'.$iteration;
+		$this->assertSame(TRUE, $input);
+		$this->assertSame($expected, Security::token(FALSE));
 	}
 
 	/**
